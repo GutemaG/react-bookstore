591b1e5d983d427607a06a50145306925e68a3e9
GutemaG <bir13gu17@gmail.com>
2023-01-23 20:24:11 +0300

    Add checke status for category pages

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import '../components/styles/Categories.css';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => (
-  <div className="category-container">
-    <button type="button" className="btn btn-status">Check status</button>
-  </div>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
+  const status = useSelector((status) => status.categoryReducer);
+  const statusChecking = () => {
+    dispatch(checkStatus());
+  };
+
+  return (
+    <div className="category-container">
+      <button type="button" onClick={statusChecking} className="btn btn-status">
+        Check status
+      </button>
+      { status ? (
+        <h3 className="category-status">{status}</h3>
+      ) : ''}
+    </div>
+  );
+};
 export default Categories;
